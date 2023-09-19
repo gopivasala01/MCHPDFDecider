2d6430b41e227445595775ccf67a47bb981a683a
gopivasala01 <gopi.v@beetlerim.com>
2023-09-19 03:58:40 -0400

    Update

--- src/main/java/mainPackage/PDFReader.java
+++ src/main/java/mainPackage/PDFReader.java
@@ -2,38 +2,55 @@ package mainPackage;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PDFReader {
 
-    public static boolean extractPDFData() {
-        try {
-            File file = RunnerClass.getLastModified();
-            System.out.println(file);
+	public static boolean extractPDFData() {
+	    try {
+	        File file = RunnerClass.getLastModified();
+	        System.out.println(file);
 
-            try (FileInputStream fis = new FileInputStream(file);
-                 PDDocument document = PDDocument.load(fis)) {
+	        try (FileInputStream fis = new FileInputStream(file);
+	             PDDocument document = PDDocument.load(fis)) {
 
-                PDFTextStripper stripper = new PDFTextStripper();
-                stripper.setStartPage(1);
-                stripper.setEndPage(1);
-                String firstPageText = stripper.getText(document);
-                String allPagesText = stripper.getText(document);
+	            PDFTextStripper stripper = new PDFTextStripper();
 
-                firstPageText = cleanText(firstPageText);
-                allPagesText = cleanText(allPagesText);
+	            // Extract text from all pages
+	            stripper.setStartPage(1);
+	            stripper.setEndPage(document.getNumberOfPages());
+	            String allPagesText = stripper.getText(document);
 
-               //System.out.println("First page text:\n" + firstPageText);
-               //System.out.println("All pages text:\n" + allPagesText);
-            }
+	            // Clean the extracted text (implement the cleanText method)
+	            allPagesText = cleanText(allPagesText);
 
-            return true; // Return true if PDF data extraction was successful
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false; // Return false if there was an error in PDF data extraction
-        }
-    }
+	            // Print the extracted text from all pages
+	            System.out.println("All pages text:\n" + allPagesText);
+
+	            System.out.println("------------------------------------------------------------------");
+
+	            String pattern = "\\d{1,2}/\\d{1,2}/\\d{4}";
+	            Pattern datePattern = Pattern.compile(pattern);
+
+	            Matcher matcher = datePattern.matcher(allPagesText);
+	            
+	            while (matcher.find()) {
+	                String matchedDate = matcher.group();
+	                System.out.println("Found date: " + matchedDate);
+	            }
+	        }
+
+	        return true; // Return true if PDF data extraction was successful
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return false; // Return false if there was an error in PDF data extraction
+	    }
+	}
+
 
     public static boolean readPDFPerMarket(String market) {
         try {
